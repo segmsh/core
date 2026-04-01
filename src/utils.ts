838086806066c5578b3ae93bf25f351006b02f64
Vadim Homchik <homchik@gmail.com>
2026-04-01 13:28:48 +0200

    Clean up

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { md5 } from "js-md5";
-import { Tag } from "./types.js";
+import { Segment } from "./types.js";
 
 export { visit } from "unist-util-visit";
 export { visitParents } from "unist-util-visit-parents";
@@ -8,10 +8,6 @@ export function hash(msg: string): string {
   return md5(msg);
 }
 
-export function id(
-  text: string,
-  tags?: { [key: string]: Tag },
-  metadata?: { [key: string]: unknown },
-): string {
+export function id({ text, tags, metadata }: Omit<Segment, "id">): string {
   return hash(JSON.stringify({ text, tags, metadata }));
 }
