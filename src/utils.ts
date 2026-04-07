68882e250086edd38c27603af9caeaf8274b50dd
Vadim Homchik <homchik@gmail.com>
2026-04-07 17:45:32 +0200

    Unified id method

--- src/utils.ts
+++ src/utils.ts
@@ -4,10 +4,6 @@ import { Segment } from "./types.js";
 export { visit } from "unist-util-visit";
 export { visitParents } from "unist-util-visit-parents";
 
-export function hash(msg: string): string {
-  return md5(msg);
-}
-
-export function id({ text, tags, metadata }: Omit<Segment, "id">): string {
-  return hash(JSON.stringify({ text, tags, metadata }));
+export function id(input: string | Omit<Segment, "id">): string {
+  return md5(typeof input === "string" ? input : JSON.stringify(input));
 }
