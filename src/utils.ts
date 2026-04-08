78f93adef4c7058df3f1f1c1b0652e38c382a32e
Vadim Homchik <homchik@gmail.com>
2026-04-08 13:24:18 +0200

    Fix non-deterministic id generation (#4)

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,30 @@
 import { md5 } from "js-md5";
-import { Segment } from "./types.js";
+import { Segment, Value } from "./types.js";
 
 export { visit } from "unist-util-visit";
 export { visitParents } from "unist-util-visit-parents";
 
+function canonicalize(value: Value): Value {
+  if (Array.isArray(value)) {
+    return value.map(canonicalize);
+  }
+
+  if (value !== null && typeof value === "object") {
+    return Object.keys(value)
+      .sort()
+      .reduce((acc, key) => {
+        acc[key] = canonicalize((value as { [key: string]: Value })[key]);
+        return acc;
+      }, {} as { [key: string]: Value });
+  }
+
+  return value;
+}
+
 export function id(input: string | Omit<Segment, "id">): string {
-  return md5(typeof input === "string" ? input : JSON.stringify(input));
+  if (typeof input === "string") {
+    return md5(input);
+  }
+
+  return md5(JSON.stringify(canonicalize(input)));
 }
